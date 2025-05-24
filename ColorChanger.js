c2bbe553ed9639a4667194ae93823309098a9aa0
pooja <poojaboppana99@gmail.com>
2025-05-24 23:59:54 +0530

    color changer

---
+++ ColorChanger.js
@@ -0,0 +1,14 @@
+function changecolor(value){
+    if(value=='green'){
+        document.body.style.backgroundColor="green";
+    }
+      else if(value=='red'){
+        document.body.style.backgroundColor="red";
+    }
+        else if(value=='blue'){
+        document.body.style.backgroundColor="blue";
+    }
+        else if(value=='yellow'){
+        document.body.style.backgroundColor="yellow";
+    }
+}
